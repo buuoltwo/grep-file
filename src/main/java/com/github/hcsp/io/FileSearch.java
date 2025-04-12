d85dabfda080341612a3883f723290bc4bf83946
buuoltwo <bullnick20@gmail.com>
2025-04-13 00:01:57 +0800

    Files.lines 能通过所有单元测试

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -2,9 +2,8 @@ package com.github.hcsp.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -13,40 +12,35 @@ public class FileSearch {
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
     public static int grep(File target, String text) {
-        try {
-            List<String> parsedLines = Files.readAllLines(target.toPath());
-            System.out.println("parsedLines = " + parsedLines);
-            for (int i = 0; i < parsedLines.size(); i++) {
-                if (parsedLines.get(i).equals(text)) {
-                    return i + 1;
-                }
-            }
-            return -1;
-        } catch (IOException e) {
-            throw new IllegalArgumentException(e);
-        }
-//        try (Stream<String> lines = Files.lines(target.toPath(), StandardCharsets.UTF_8)) {
-//            final AtomicInteger lineNum = new AtomicInteger(1);
-//            return lines
-//                    .sequential() // 确保行号顺序
-//                    .filter(line -> {
-//                        boolean match = line.equals(text);
-//                        if (!match) {
-//                            lineNum.incrementAndGet();
-//                        }
-//                        return match;
-//                    })
-//                    .mapToInt(line -> lineNum.get())
-//                    .findFirst()
-//                    .orElse(-1);
-//        } catch (IOException e) {
-//            throw new IllegalArgumentException("读取文件失败: " + e.getMessage(), e);
+//        if (!target.exists() || !target.canRead()) {
+//            throw new IllegalArgumentException();
 //        }
+        AtomicInteger myIndex = new AtomicInteger(0);
+        try {
+            Stream<String> lines = null;
+            lines = Files.lines(target.toPath());
+//            lines.forEach(System.out::println);
+            Optional<Integer> integerStream = lines
+                    .map(line -> {
+                        int curIndex = myIndex.incrementAndGet();
+                        if (line.contains(text)) {
+                            return myIndex.get();
+                        }
+                        return -1;
+                    })
+                    .filter(num -> num != -1)
+                    .findFirst();
+            return integerStream.orElse(-1);
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            throw new IllegalArgumentException();
+        }
 
+//        return 0;
     }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
-        System.out.println("结果行号：" + grep(new File(projectDir, "log.txt"), "BBBB"));
+        System.out.println("结果行号：" + grep(new File(projectDir, "log.txt"), "BBB"));
     }
 }
